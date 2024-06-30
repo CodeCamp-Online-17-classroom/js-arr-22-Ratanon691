af27ddc0c9ef71ae1557888a3635bab69a897966
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-30 17:43:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// write code here
+function makeWorker() {
+  let name = 'Pete';
+  return function () {
+    alert(name);
+  };
+}
+let name = 'John';
+let work = makeWorker();
+work(); // alert `Pete` เพราะ work มีค่าเป็นfunction makeWorker() ซึ่งในfunction นี้มีname = `Pete` เป็นlocalอยู่แล้ว และเมื่อalert จะalertแค่ตัวแปรจากfunctionนั้นๆ ไม่เกี่ยวกับglobal variable
